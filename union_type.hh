2392bc68d6deb26461475833780c8d0f398f1332
sys-fork_desktop <minato.sysfork@gmail.com>
2024-03-02 01:36:08 +0900

    Implement holds_alternative

--- union_type.hh
+++ union_type.hh
@@ -3,104 +3,149 @@
 #include <iostream>
 #include <cstdlib>
 
-template <class...>
-struct Union;
+namespace union_type {
+    template <class...>
+    struct Union;
 
-template <class U>
-struct Union<U> {
-private:
-    U u;
-    bool u_is_set;
-public:
-    Union() = default;
-    Union(const U& u_) : u(u_), u_is_set(true) {}
-    // コピーコンストラクタはデフォルト
-    Union(U&& u_) : u(std::move(u_)), u_is_set(true) {}
-    // ムーブコンストラクタはデフォルト
+    template <class U>
+    struct Union<U> {
+    private:
+        U u;
+        bool u_is_set;
+    public:
+        Union() = default;
+        Union(const U& u_) : u(u_), u_is_set(true) {}
+        // コピーコンストラクタはデフォルト
+        Union(U&& u_) : u(std::move(u_)), u_is_set(true) {}
+        // ムーブコンストラクタはデフォルト
 
-    Union& operator=(const U& u_) {
-        u = u_;
-        u_is_set = true;
-        return *this;
-    }
-    // コピー代入演算子はデフォルト
-    Union& operator=(U&& u_) {
-        u = std::move(u_);
-        u_is_set = true;
-        return *this;
-    }
-    // ムーブ代入演算子はデフォルト
-
-    operator U() const noexcept {
-        if (!u_is_set) {
-            std::cerr
-                << "Union Type Error: Required type "
-                << typeid(U).name()
-                << " is not set." << std::endl;
-            std::exit(EXIT_FAILURE);
+        Union& operator=(const U& u_) {
+            u = u_;
+            u_is_set = true;
+            return *this;
         }
-        return u;
-    }
-
-    virtual ~Union() = default;
-};
-
-template <class U, class V, class ...Rest>
-struct Union<U, V, Rest...> {
-private:
-    U u;
-    Union<V, Rest...> v;
-    bool u_is_set = false;
-    bool v_is_set = false;
-
-public:
-    Union() = default;
-
-    template <class T, std::enable_if_t<!std::is_same_v<T, U>, std::nullptr_t> = nullptr>
-    Union(T&& t_) : v(std::forward<T>(t_)) {}
-
-    template <class T, std::enable_if_t<std::is_same_v<T, U>, std::nullptr_t> = nullptr>
-    Union(T&& t_) : u(std::forward<T>(t_)), u_is_set(true) {}
-
-    template <class T, std::enable_if_t<!std::is_same_v<std::decay_t<T>, U>, std::nullptr_t> = nullptr>
-    Union& operator=(T&& t_) {
-        v = std::forward<T>(t_);
-        u_is_set = false;
-        v_is_set = true;
-        return *this;
-    }
-
-    template <class T, std::enable_if_t<std::is_same_v<std::decay_t<T>, U>, std::nullptr_t> = nullptr>
-    Union& operator=(T&& t_) {
-        u = std::forward<T>(t_);
-        u_is_set = true;
-        v_is_set = false;
-        return *this;
-    }
-
-    template <class T, std::enable_if_t<!std::is_same<std::decay_t<T>, U>::value, std::nullptr_t> = nullptr>
-    operator T() const noexcept {
-        if (!v_is_set) {
-            std::cerr
-                << "Union Type Error: Required type "
-                << typeid(T).name()
-                << " is not set." << std::endl;
-            std::exit(EXIT_FAILURE);
+        // コピー代入演算子はデフォルト
+        Union& operator=(U&& u_) {
+            u = std::move(u_);
+            u_is_set = true;
+            return *this;
         }
-        return static_cast<T>(v);
-    }
+        // ムーブ代入演算子はデフォルト
 
-    template <class T, std::enable_if_t<std::is_same<std::decay_t<T>, U>::value, std::nullptr_t> = nullptr>
-    operator T() const noexcept {
-        if (!u_is_set) {
-            std::cerr
-                << "Union Type Error: Required type "
-                << typeid(T).name()
-                << " is not set." << std::endl;
-            std::exit(EXIT_FAILURE);
+        operator U() const {
+            if (!u_is_set) {
+                std::cerr
+                    << "Union Type Error: Required type "
+                    << typeid(U).name()
+                    << " is not set." << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
+            return u;
         }
-        return u;
+
+        template <class T, class S>
+        friend auto holds_alternative(Union<S>) noexcept
+            -> std::enable_if_t<!std::is_same_v<std::decay_t<T>, U>, bool>;
+
+        template <class T, class S>
+        friend auto holds_alternative(Union<S>) noexcept
+            -> std::enable_if_t<std::is_same_v<std::decay_t<T>, U>, bool>;
+
+        virtual ~Union() = default;
+    };
+
+    template <class T, class U>
+    auto holds_alternative(const Union<U>&) noexcept
+        -> std::enable_if_t<!std::is_same_v<std::decay_t<T>, U>, bool> {
+        return false;
     }
 
-    virtual ~Union() = default;
-};
+    template <class T, class U>
+    auto holds_alternative(const Union<U>& u)
+        -> std::enable_if_t<std::is_same_v<std::decay_t<T>, U>, bool> {
+        return u.u_is_set;
+    }
+
+    template <class U, class V, class ...Rest>
+    struct Union<U, V, Rest...> {
+    private:
+        U u;
+        Union<V, Rest...> v;
+        bool u_is_set = false;
+        bool v_is_set = false;
+
+    public:
+        Union() = default;
+
+        template <class T, std::enable_if_t<!std::is_same_v<T, U>, std::nullptr_t> = nullptr>
+        Union(T&& t_) : v(std::forward<T>(t_)), v_is_set(true) {}
+
+        template <class T, std::enable_if_t<std::is_same_v<T, U>, std::nullptr_t> = nullptr>
+        Union(T&& t_) : u(std::forward<T>(t_)), u_is_set(true) {}
+
+        template <class T, std::enable_if_t<!std::is_same_v<std::decay_t<T>, U>, std::nullptr_t> = nullptr>
+        Union& operator=(T&& t_) {
+            v = std::forward<T>(t_);
+            u_is_set = false;
+            v_is_set = true;
+            return *this;
+        }
+
+        template <class T, std::enable_if_t<std::is_same_v<std::decay_t<T>, U>, std::nullptr_t> = nullptr>
+        Union& operator=(T&& t_) {
+            u = std::forward<T>(t_);
+            u_is_set = true;
+            v_is_set = false;
+            return *this;
+        }
+
+        template <class T, std::enable_if_t<!std::is_same<std::decay_t<T>, U>::value, std::nullptr_t> = nullptr>
+        operator T() const {
+            if (!v_is_set) {
+                std::cerr
+                    << "Union Type Error: Required type "
+                    << typeid(T).name()
+                    << " is not set." << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
+            // ここで static_cast<T>(v) としてもよいが，operator T() の戻り値は
+            // T なので，このまま v を返しても暗黙の型変換により T 型の値に変換される．
+            return v;
+        }
+
+        template <class T, std::enable_if_t<std::is_same<std::decay_t<T>, U>::value, std::nullptr_t> = nullptr>
+        operator T() const {
+            if (!u_is_set) {
+                std::cerr
+                    << "Union Type Error: Required type "
+                    << typeid(T).name()
+                    << " is not set." << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
+            return u;
+        }
+
+        template <class T, class S, class W, class ...Rest_types>
+        friend auto holds_alternative(const Union<S, W, Rest_types...>&) noexcept
+            -> std::enable_if_t<!std::is_same_v<std::decay_t<T>, S>, bool>;
+
+        template <class T, class S, class W, class ...Rest_types>
+        friend auto holds_alternative(const Union<S, W, Rest_types...>&) noexcept
+            -> std::enable_if_t<std::is_same_v<std::decay_t<T>, S>, bool>;
+
+        virtual ~Union() = default;
+    };
+
+    template <class T, class U, class V, class ...Rest>
+    auto holds_alternative(const Union<U, V, Rest...>& u) noexcept
+        -> std::enable_if_t<!std::is_same_v<std::decay_t<T>, U>, bool> {
+        return u.v_is_set && holds_alternative<T>(u);
+    }
+
+    template <class T, class U, class V, class ...Rest>
+    auto holds_alternative(const Union<U, V, Rest...>& u) noexcept
+        -> std::enable_if_t<std::is_same_v<std::decay_t<T>, U>, bool> {
+        return u.u_is_set;
+    }
+
+} // namespace union_type
--- union_type_test.cc
+++ union_type_test.cc
@@ -2,25 +2,37 @@
 #include <string>
 #include "union_type.hh"
 
-void f(int i) {
+void f(int i)
+{
     std::cout << i << std::endl;
 }
 
-void g(std::string s) {
+void g(std::string s)
+{
     std::cout << s << std::endl;
+}
+
+void h(union_type::Union<int, std::string> u)
+{
+    if (union_type::holds_alternative<int>(u)) {
+        std::cout << static_cast<int>(u) + 1 << std::endl;
+    } else {
+        std::cout << "You said: " + static_cast<std::string>(u) << std::endl;
+    }
 }
 
 int main(void)
 {
-    Union<int, std::string, float, std::uint8_t, std::uint32_t> u = 1;
-    Union<int> i = 3;
+    union_type::Union<int, std::string, float,
+        std::uint8_t, std::uint32_t> u = 1;
+    union_type::Union<int> i = 2;
 
     u = 1;
     f(u);
-    // Union の実装の内部で std::is_same_v で場合分けしている
-    // 関係で，std::string 型にしないとうまく動かない
     u = std::string("Hello World!");
     g(u);
     // f(u); -> Error
     f(i);
+    h(3);
+    h(std::string("Have a nice day!"));
 }
