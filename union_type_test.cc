9a40a7204cdea3bd955724e775291a7cb72d5b93
sys-fork_desktop <minato.sysfork@gmail.com>
2024-03-01 23:02:14 +0900

    Fix some bugs

--- union_type_test.cc
+++ union_type_test.cc
@@ -12,12 +12,14 @@ void g(std::string s) {
 
 int main(void)
 {
-    Union<int, std::string, float> u = 1;
+    Union<int, std::string, float, std::uint8_t, std::uint32_t> u = 1;
     Union<int> i = 3;
 
     u = 1;
     f(u);
-    u = "Hello, world!";
+    // Union の実装の内部で std::is_same_v で場合分けしている
+    // 関係で，std::string 型にしないとうまく動かない
+    u = std::string("Hello World!");
     g(u);
     // f(u); -> Error
     f(i);
